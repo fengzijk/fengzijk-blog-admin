ce583bd74a91cda9e5934a713cf53293dd0e7852
fengzijk <guozhifengvip@163.com>
2022-05-02 03:30:30 +0800

    Initial commit

---
+++ src/router/modules/components.js
@@ -0,0 +1,18 @@
+/** When your routing table is too long, you can split it into small modules **/
+
+import Layout from '@/layout'
+
+const componentsRouter = {
+  path: '/components',
+  component: Layout,
+  redirect: 'noRedirect',
+  name: 'ComponentDemo',
+  meta: {
+    title: 'components',
+    icon: 'component'
+  },
+  children: [
+  ]
+}
+
+export default componentsRouter
